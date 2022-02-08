502aa4d8511b228fb42bfaf9d84fcd611a12ef0c
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2022-02-08 11:14:45 -0700

    wip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className='contact'>
             <h2>Get in Touch!</h2>
             <a href="mailto:051jvqstj@mozmail.com?subject=
-                    I%20Am%20Interested%20in%20working%20together"
+                    I%20am%20interested%20in%20working%20together"
                     target="_top">
             <button >Contact</button></a>
         </div>
